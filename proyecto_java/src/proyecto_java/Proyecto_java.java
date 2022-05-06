9667b3c10360d03e5d63644b144771de1c4530a0
ManuelPiano <manuel.alvarado21@itca.edu.sv>
2022-05-06 03:07:50 -0600

    creacion del segundo commit con sourcetree

--- proyecto_java/src/proyecto_java/Proyecto_java.java
+++ proyecto_java/src/proyecto_java/Proyecto_java.java
@@ -14,7 +14,9 @@ public class Proyecto_java {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("creacion del primer commit");
+        
+
     }
     
 }
